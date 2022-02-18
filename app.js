810ef42fa7ad4c6d96f912163b676f5d3dd14af2
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-02-18 12:21:37 +0100

    creating routes for login and registration

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
+// BodyParser middlewear 
+app.use(bodyParser. urlencoded({extended: true}))
+
+// Parse application/json
+app.use(bodyParser.json())
+
+// set template engine 
+app.set("view engine", "ejs")
 
 
-
-
-
-
-
+// route for root page
+app.get("/", (req,res) => {
+    res.render("Login")
+})
 
 app.listen(port, () => {
     console.log(`Port started on ${port}`)

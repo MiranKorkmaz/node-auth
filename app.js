259a64f2355dce4de31e6afb7dc566f0fd1257f6
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-02-18 13:08:35 +0100

    initalizing registration and login

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
+
+// serving static files 
+app.use(express.static("public"))
+
+// connect app to mongoose 
+mongoose.connect("mongodb://localhost/users", {
+}).then(console.log("mongoDB connected")).catch(err => console.log(err))
+
+// import User model
+const User = require("./models/User")
 
 // BodyParser middlewear 
 app.use(bodyParser. urlencoded({extended: true}))
@@ -12,10 +23,14 @@ app.use(bodyParser.json())
 // set template engine 
 app.set("view engine", "ejs")
 
-
-// route for root page
-app.get("/", (req,res) => {
+// route for login page
+app.get("/login", (req,res) => {
     res.render("Login")
+})
+
+// route for registration page 
+app.get("/register", (req,res) => {
+    res.render("Register")
 })
 
 app.listen(port, () => {
